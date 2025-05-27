dfbc17d4b7bfad0530fcdc477c3d23a8e85f6ba8
Ali Moghimi <mghmiali@gmail.com>
2025-05-27 20:06:29 +0000

    chore: add hard coded URL endpoint

--- scraper/engine.py
+++ scraper/engine.py
@@ -6,16 +6,18 @@ from utils import Request
 
 
 class Engine:
+
+    URL = "https://www.ice.com/api/cds-settlement-prices/icc-single-names"
+
     def __init__(self, url: str, max_retries: int, backoff_factor: int) -> None:
-        self.url = url
         self.request = Request(max_retries=max_retries, backoff_factor=backoff_factor)
 
     def fetch(self, content: str = None) -> pd.DataFrame:
-        logger.debug(f"Attempting to fetch content from {self.url}.")
+        logger.debug(f"Attempting to fetch content from {self.URL}.")
         if not content:
             data = self.get_content()
             logger.debug(
-                f"Successfully fetched content from {self.url}. Now parsing the content."  # noqa: E501
+                f"Successfully fetched content from {self.URL}. Now parsing the content."  # noqa: E501
             )
         else:
             logger.debug(
@@ -25,17 +27,17 @@ class Engine:
         df = self.convert_to_df(data)
         logger.debug(f"\n{df}")
         self.validate_data(df)
-        logger.info(f"Parsed content from {self.url}. Extracted {len(df)} rows.")
+        logger.info(f"Parsed content from {self.URL}. Extracted {len(df)} rows.")
         return df
 
     def get_content(self) -> str:
         try:
-            r = self.request.request("GET", self.url)
+            r = self.request.request("GET", self.URL)
             r.raise_for_status()
             return r.json()
         except requests.RequestException as e:
-            logger.error(f"Error fetching content from {self.url}. Error: {e}")
-            raise ConnectionError(f"Failed to connect to {self.url}.") from e
+            logger.error(f"Error fetching content from {self.URL}. Error: {e}")
+            raise ConnectionError(f"Failed to connect to {self.URL}.") from e
 
     def convert_to_df(self, data: list) -> pd.DataFrame:
         try:
@@ -46,11 +48,11 @@ class Engine:
 
         if len(df):
             logger.debug(
-                f"Successfully parsed content from {self.url}. Extracted {len(df)} rows."  # noqa: E501
+                f"Successfully parsed content from {self.URL}. Extracted {len(df)} rows."  # noqa: E501
             )
             return df
         else:
-            raise ValueError(f"No data found when parsing content from {self.url}.")
+            raise ValueError(f"No data found when parsing content from {self.URL}.")
 
     def validate_data(self, df: pd.DataFrame) -> None:
         if df["clearingDate"].str.contains("No data found").any() or not len(df):
